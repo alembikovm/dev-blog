6e4196903dab4bbe486095226dcf748e7a54c2a8
Marat Alembikov <m.alembikov@ritm.media>
2021-04-23 10:55:15 +0400

    Added mobile checking

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import Head from "next/head";
 import { Typography } from "antd";
+import { useEffect, useState } from "react";
+
+import { isMacOs, isWindows, isWinPhone } from "react-device-detect";
 
 import Layout, { siteTitle } from "../components/layout.tsx";
 import { getSortedPostsData } from "../lib/posts";
@@ -19,35 +22,44 @@ export async function getStaticProps() {
 
 export default function Home({ allPostsData }) {
   const { Title, Text } = Typography;
+  const [isCriOs, setisCriOs] = useState(false);
+  useEffect(() => {
+    setisCriOs(/CriOS/i.test(navigator.userAgent));
+  }, []);
+  if ((isMacOs && !isCriOs) || (isWindows && !isWinPhone)) {
+    return <h1>PC</h1>;
+  } else {
+    return <h1>Mobile</h1>;
+  }
 
-  return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
+  // return (
+  //   <Layout home>
+  //     <Head>
+  //       <title>{siteTitle}</title>
+  //     </Head>
 
-      <Typography>
-        <section className={utilStyles.headingMd}>
-          <Text>Hi, I'm Software Engineer. I solve problems.</Text>
-        </section>
+  //     <Typography>
+  //       <section className={utilStyles.headingMd}>
+  //         <Text>Hi, I'm Software Engineer. I solve problems.</Text>
+  //       </section>
 
-        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-          <Title level={2}>Blog</Title>
-          <ul className={utilStyles.list}>
-            {allPostsData.map(({ id, date, title }) => (
-              <li className={utilStyles.listItem} key={id}>
-                <Link href={`/posts/${id}`}>
-                  <a>{title}</a>
-                </Link>
-                <br />
-                <small className={utilStyles.lightText}>
-                  <Date dateString={date} />
-                </small>
-              </li>
-            ))}
-          </ul>
-        </section>
-      </Typography>
-    </Layout>
-  );
+  //       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+  //         <Title level={2}>Blog</Title>
+  //         <ul className={utilStyles.list}>
+  //           {allPostsData.map(({ id, date, title }) => (
+  //             <li className={utilStyles.listItem} key={id}>
+  //               <Link href={`/posts/${id}`}>
+  //                 <a>{title}</a>
+  //               </Link>
+  //               <br />
+  //               <small className={utilStyles.lightText}>
+  //                 <Date dateString={date} />
+  //               </small>
+  //             </li>
+  //           ))}
+  //         </ul>
+  //       </section>
+  //     </Typography>
+  //   </Layout>
+  // );
 }
